a3a629bb2ff028f2187281991ca285aea4dba392
galshaydemo <galshay24@gmail.com>
2020-08-14 03:13:21 +0300

    elements

--- App.tsx
+++ App.tsx
@@ -1,26 +1,19 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, StatusBar } from "react-native"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import List from './List';
 import Detail from './Detail';
-const Stack = createStackNavigator();
+import About from './About';
+import { Routes } from './Routes';
+
 const App = () => {
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="List"
-          component={List}
-          options={{ title: 'Randomize Me!' }}
-        />
-        <Stack.Screen name="Detail" component={Detail} />
-      </Stack.Navigator>
-      {/* Rest of your app code */}
-    </NavigationContainer>
-  );
+  return <Routes />
+
+
+
 };
 
 export default App;
--- List.tsx
+++ List.tsx
@@ -6,9 +6,9 @@
  * @flow strict-local
  */
 
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import 'react-native-gesture-handler';
-
+import { ListItem } from 'react-native-elements';
 import {
     SafeAreaView,
     StyleSheet,
@@ -24,6 +24,7 @@ import axios from 'axios';
 import { Result } from './User';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Detail from './Detail';
+import { useNavigation } from '@react-navigation/native';
 
 export interface Props {
     navigation: any;
@@ -35,104 +36,121 @@ interface State {
     loading: boolean;
     data: Array<Result>
 }
-class List extends Component<Props, State> {
-    _data = Array<Result>();
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            data: [],
-            loading: true
+const FlatListItemSeparator = () => {
+    return (
+        <View
+            style={{
+                height: 1,
+                width: "100%",
+                backgroundColor: "#000",
+            }}
+        />
+    );
+}
+
+const show = (data: Array<Result>) => {
+    const navigation = useNavigation();
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <FlatList<Result> data={data}
+                keyExtractor={(item) => item.login.uuid}
+                ItemSeparatorComponent={FlatListItemSeparator}
+                renderItem={({ item }) =>
+                    <TouchableOpacity onPress={() => {
+
+                        navigation.navigate('Detail',
+                            { data: item, title: item.name.first + ' ' + item.name.last });
+
+
+                    }}>
+
+
+                        <View style={{ flexDirection: 'row', height: 30, marginVertical: 5, paddingLeft: 10, backgroundColor: '#E0E0E0' }} >
+                            <Text style={styles.text}>{item.name.title}</Text>
+                            <Text style={styles.text}>{item.name.first}</Text>
+                            <Text style={styles.text}>{item.name.last}</Text>
+                        </View>
+                    </TouchableOpacity>}
+            >
+
+            </FlatList>
+            <SafeAreaView></SafeAreaView>
+        </>
+
+    );
+}
+const waiting = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <View style={[styles.container, styles.horizontal]}>
+                <ActivityIndicator size="large" color="#00ff00" />
+            </View>
+            <SafeAreaView></SafeAreaView>
+        </>);
+}
+const oneCall = async (): Promise<Result | undefined> => {
+    let p = undefined;
+    await axios.get('https://randomuser.me/api')
+        .then((response) => {
+            console.log(response.data.results[0])
+            p = response.data.results[0] as Result;
+
+        }, (error) => {
+            console.log("error " + error);
+            return undefined
+        });
+
+    return p
+
+}
+
+
+
+const List = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(Array<Result>());
+    const loadData = async () => {
+        let localData = [];
+        for (var i: number = 0; localData.length < 10; i++) {
+            const response = await axios.get('https://randomuser.me/api');
+            localData.push(response.data.results[0])
         }
-    }
-    async oneCall(i: number) {
-        try {
-
-            await axios.get('https://randomuser.me/api')
-                .then((response) => {
-                    this._data.push(response.data.results[0])
-                    //this.setState({ data: this.state.data })
-                    //console.log(myData.length)
-                }, (error) => {
-                    console.log("error " + error);
-                });
-
-
-            //Assign the promise unresolved first then get the data using the json method. 
-
-            //this.setState({pokeList: pokemon, loading: false});
-        } catch (err) {
-            console.log("Error fetching data-----------", err);
-        }
-
-    }
-    async componentDidMount() {
-        var myData = new Array();
-        for (let i: number = 0; this._data.length < 10; i++) {
-            await this.oneCall(i);
-
-        }
-        this.setState({ data: this._data, loading: false })
-    }
-    FlatListItemSeparator = () => {
-        return (
-            <View
-                style={{
-                    height: 1,
-                    width: "100%",
-                    backgroundColor: "#000",
-                }}
-            />
-        );
-    }
-    waiting = () => {
-        return (
-            <>
-                <StatusBar barStyle="dark-content" />
-                <View style={[styles.container, styles.horizontal]}>
-                    <ActivityIndicator size="large" color="#00ff00" />
-                </View>
-                <SafeAreaView></SafeAreaView>
-            </>);
-    }
-    show = () => {
-        return (
-            <>
-                <StatusBar barStyle="dark-content" />
-                <FlatList<Result> data={this.state.data}
-                    keyExtractor={(item) => item.login.uuid}
-                    ItemSeparatorComponent={this.FlatListItemSeparator}
-                    renderItem={({ item }) =>
-                        <TouchableOpacity onPress={() => {
-
-                            this.props.navigation.navigate('Detail',
-                                { data: item, title: item.name.first + ' ' + item.name.last });
-
-                            //console.log('Tap me' + this.props.navigation);
-                        }}>
-                            <View style={{ flexDirection: 'row', height: 30, marginVertical: 5, paddingLeft: 10, backgroundColor: '#E0E0E0' }} >
-                                <Text style={styles.text}>{item.name.title}</Text>
-                                <Text style={styles.text}>{item.name.first}</Text>
-                                <Text style={styles.text}>{item.name.last}</Text>
-                            </View></TouchableOpacity>}
-                >
-
-                </FlatList>
-                <SafeAreaView></SafeAreaView>
-            </>
-
-        );
+        setData(localData);
+        setLoading(false)
     }
 
-    render() {
-        return (this.state.loading ? this.waiting() : this.show())
+    useEffect(() => {
+        loadData();
+    }, [loading]);
+
+
+    //return <View><Text>List</Text></View>
+    return (loading ? waiting() : show(data))
 
 
 
 
-    }
+}
+
+//setData(_data);
+//this.setState({ data: this._data, loading: false })
 
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
     text:
